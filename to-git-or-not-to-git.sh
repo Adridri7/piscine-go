0b523777abc9f409a76b39fbbe4141b33d0098d3
Adridri7 <adrieen.rocchetti@gmail.com>
2024-02-26 11:15:08 +0100

    My very first commit

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,3 +1,5 @@
-echo 'Chameleon'
-echo '28'
-echo 'Male'
+jq  -r '.[] | select(.id==170) | .name ' all.json
+jq -r '.[] | select(.id==170) | .powerstats.power ' all.json
+jq  -r '.[] | select(.id==170) | .appearance.gender ' all.json
+
+
