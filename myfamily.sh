e0dfbb68d9ccc7769c729a95962a39b4e54da947
Adridri7 <adrieen.rocchetti@gmail.com>
2024-02-26 15:44:13 +0100

    Update du fichier myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl --silent "https://zone01normandie.org/assets/superhero/all.json" | jq -r --argjson hero_id $HERO_ID '.[] | select(.id == $hero_id) | .connections.relatives | gsub("\n"; "")'
+curl --silent "https://zone01normandie.org/assets/superhero/all.json" | jq -r --argjson hero_id $HERO_ID '.[] | select(.id == $hero_id) | .connections.relatives | gsub("\n"; "\\n")'
