5afee483d209cf2773210c4b7c25596136919ce9
Adridri7 <adrieen.rocchetti@gmail.com>
2024-02-28 18:06:28 +0100

    Update

--- printnbr.go
+++ printnbr.go
@@ -7,7 +7,7 @@ import (
 var r []rune
 
 func PrintNbr(n int) {
-	var n1 uint16
+	var n1 uint64
 	if n < 0 {
 		z01.PrintRune('-')
 		n *= -1
@@ -17,7 +17,7 @@ func PrintNbr(n int) {
 		z01.PrintRune('0')
 	}
 
-	n1 = uint16(n)
+	n1 = uint64(n)
 
 	for n1 != 0 {
 		r = append(r, rune(n1%10+'0'))
@@ -27,4 +27,5 @@ func PrintNbr(n int) {
 	for i := len(r) - 1; 0 <= i; i-- {
 		z01.PrintRune(r[i])
 	}
+	z01.PrintRune(' ')
 }
