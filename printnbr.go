386c741f3a86827e33c229cba1088bfd04bb310d
Adridri7 <adrieen.rocchetti@gmail.com>
2024-02-28 15:39:04 +0100

    Ajout du fichier printnbr.go

--- printnbr.go
+++ printnbr.go
@@ -1,17 +1,43 @@
 package piscine
 
 import (
-	"strconv"
-
 	"github.com/01-edu/z01"
 )
 
 func PrintNbr(n int) {
-	for _, char := range Int_to_str(n) {
-		z01.PrintRune(char)
+	for _, val := range intToString(n) {
+		z01.PrintRune(val)
 	}
 }
 
-func Int_to_str(n int) string {
-	return strconv.Itoa(n)
+func intToString(num int) string {
+	if num == 0 {
+		return "0"
+	}
+
+	var result []rune
+
+	isNegative := false
+	if num < 0 {
+		isNegative = true
+		num = -num
+	}
+
+	for num != 0 {
+		digit := num % 10
+		result = append(result, rune(digit+'0'))
+		num /= 10
+	}
+
+	if isNegative {
+		result = append(result, '-')
+	}
+
+	// Inverse la chaîne résultante
+	length := len(result)
+	for i := 0; i < length/2; i++ {
+		result[i], result[length-i-1] = result[length-i-1], result[i]
+	}
+
+	return string(result)
 }
