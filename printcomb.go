93f918e0ec7305494ff37c402ccfc88a013be148
root <dev@01-edu.org>
2024-02-28 10:02:32 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -1,16 +1,22 @@
 package piscine
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
-func PrintComb(){
-  for i := 0; i < 10; i++ {
-        for j := i + 1; j < 10; j++ {
-            for k := j + 1; k < 10; k++ {
-                fmt.Printf("%d%d%d", i, j, k)
-                if !(i == 7 && j == 8 && k == 9) {
-                    fmt.Print(", ")
-                }
-            }
-        }
-    }
+func PrintComb() {
+	for a := '0'; a <= '7'; a++ {
+		for b := a + 1; b <= '8'; b++ {
+			for c := b + 1; c <= '9'; c++ {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
+				if a != '7' || b != '8' || c != '9' {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+		}
+	}
+
 }
