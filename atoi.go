8e5fb76a52cdc867d53c9232c10d2efcb9067ad8
root <dev@01-edu.org>
2024-02-29 11:20:53 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -4,6 +4,18 @@ func Atoi(str string) int {
 	result := 0
 	sign := 1
 
+    if len(str) == 0 {
+		return 0 // Retourner 0 si la chaîne est vide
+	}
+
+    if len(str) == 1 {
+        digit := int(str[0] - '0')
+		result = result*10 + digit
+		return 0 // Retourner 0 si la chaîne est vide
+	}
+
+  
+
 	// Gérer le signe
 	if str[0] == '-' {
 		sign = -1
