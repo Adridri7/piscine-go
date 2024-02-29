d51dd793f5416501223354673e85846ddb9c23d6
Adridri7 <adrieen.rocchetti@gmail.com>
2024-02-29 11:03:29 +0100

    Ajout du fichier basicatoic2.go

---
+++ basicatoi2.go
@@ -0,0 +1,15 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	result := 0
+
+	for _, char := range s {
+		if char <= '0' || char >= '9' {
+			return 0
+		}
+		digit := int(char - '0')
+		result = result*10 + digit
+	}
+
+	return result
+}
