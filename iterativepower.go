fdf1d35e3471757934869401b34cca7b1b90ca80
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-04 09:37:26 +0100

    Ajout du fichier iterativepower.go

---
+++ iterativepower.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	} else if power == 0 {
+		return 1
+	}
+	res := 1
+	for i := 1; i <= power; i++ {
+		res *= nb
+	}
+	return res
+}
