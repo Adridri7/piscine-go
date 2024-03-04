635025e79e8dda2310b39e14786dbed4a2baf79c
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-04 10:16:12 +0100

    Ajout du fichier sqrt.go

---
+++ sqrt.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb == 0 || nb < 0 {
+		return 0
+	}
+
+	z := nb / 2
+	for z*z-nb > 0 {
+		z = (z + nb/z) / 2
+	}
+
+	if z*z != nb {
+		return 0
+	}
+
+	return z
+}
