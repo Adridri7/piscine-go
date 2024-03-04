8cf8d0ad2fade3776178f0922a08ef87c0ab61a2
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-04 15:38:51 +0100

    Update

--- isprime.go
+++ isprime.go
@@ -4,14 +4,14 @@ func IsPrime(n int) bool {
 	if n <= 1 {
 		return false
 	}
-	if n <= 3 || n == 5 || n == 7 {
+	if n <= 3 || n == 5 {
 		return true
 	}
 	if n%2 == 0 || n%3 == 0 {
 		return false
 	}
-	for i := 10; i*i <= n; i += 6 {
-		if n%i == 0 {
+	for i := 5; i*i <= n; i += 6 {
+		if n%i == 0 || n%(i+2) == 0 {
 			return false
 		}
 	}
