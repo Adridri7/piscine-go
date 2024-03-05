c81cf334a28ed8ced99641b5619a9347f77a37df
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-05 11:03:17 +0100

    Update

--- isalpha.go
+++ isalpha.go
@@ -1,7 +1,6 @@
 package piscine
 
 func IsAlpha(s string) bool {
-
 	for _, val := range s {
 		if !(val >= 'A' && val <= 'z') && !(val >= '0' && val <= '9') {
 			return false
--- test/main.go
+++ test/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IsAlpha("Hello! How are you?"))
+	fmt.Println(piscine.IsAlpha("a"))
 	fmt.Println(piscine.IsAlpha("HelloHowareyou"))
 	fmt.Println(piscine.IsAlpha("What's this 4?"))
 	fmt.Println(piscine.IsAlpha("Whatsthis4"))
