5f2bab707724e5efe2dc843f221c714a5061fe43
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-06 09:16:29 +0100

    update

--- test/main.go
+++ test/main.go
@@ -17,11 +17,9 @@ func main() {
 	//piscine.PrintNbrBase(-125, "choumi")
 	//z01.PrintRune('\n')
 	//piscine.PrintNbrBase(-9223372036854775808, "0123456789")
-
 	fmt.Println(piscine.BasicAtoi2("12345"))
 	fmt.Println(piscine.BasicAtoi2("0000000012345"))
 	fmt.Println(piscine.BasicAtoi2("012 345"))
 	fmt.Println(piscine.BasicAtoi2("Hello World!"))
-
 	z01.PrintRune('\n')
 }
