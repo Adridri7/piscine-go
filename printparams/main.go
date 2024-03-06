86c2e8492b83257228dbea4b6fb3ca64b8f12571
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-06 09:43:32 +0100

    Update

--- printparams/main.go
+++ printparams/main.go
@@ -9,12 +9,12 @@ import (
 func main() {
 	args := os.Args
 	for idx, arg := range args {
-		for _, char := range arg {
-			if char != '/' && char != '.' {
-				z01.PrintRune(char)
+		if idx != 0 {
+			for _, char := range arg {
+				if char != '/' && char != '.' {
+					z01.PrintRune(char)
+				}
 			}
-		}
-		if idx != len(args)-1 {
 			z01.PrintRune('\n')
 		}
 	}
