cfbb0a6a316fbe73aad8bcfd42e4315df3cfdbce
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-07 09:17:54 +0100

    Update

--- makerange.go
+++ makerange.go
@@ -2,9 +2,8 @@ package piscine
 
 func MakeRange(min, max int) []int {
 	tab := make([]int, max)
-	for i := min; i < max; i++ {
+	for i := min; i <= max; i++ {
 		tab[i] = i + 1
 	}
-
 	return tab
 }
