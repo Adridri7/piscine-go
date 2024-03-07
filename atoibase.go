e4a40a531d4b55cb2ddd2e8f7611171931056ebe
root <dev@01-edu.org>
2024-03-07 15:10:01 +0000

    Update 'atoibase.go'

--- atoibase.go
+++ atoibase.go
@@ -2,9 +2,11 @@ package piscine
 
 func AtoiBase(s string, base string) int {
   res := 0
+  if !isValidBase(base) {
+		return res
+  }
   cpt := len(s)-1
   idx := 0
-  
   for _, char := range s{
     for ind, c := range base {
       if char == c{
@@ -14,10 +16,8 @@ func AtoiBase(s string, base string) int {
     res += idx*RecursivePower(len(base), cpt)
     cpt--
   }
-  
   return res
 }
-
 
 func RecursivePower(nb int, power int) (res int) {
 	if power == 0 {
@@ -27,4 +27,28 @@ func RecursivePower(nb int, power int) (res int) {
 	} else {
 		return nb * RecursivePower(nb, power-1)
 	}
+}
+
+func isValidBase(base string) bool {
+	if len(base) < 2 {
+		return false
+	}
+	for _, val := range base {
+		if val == '-' || val == '+' {
+			return false
+		} else if !contain1(base, val) {
+			return false
+		}
+	}
+	return true
+}
+
+func contain1(base string, r rune) bool {
+	cpt := 0
+	for _, val := range base {
+		if val == r {
+			cpt++
+		}
+	}
+	return cpt == 1
 }
