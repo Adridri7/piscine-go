b6fa4957f95929f19be26ef46e09912133bc401d
root <dev@01-edu.org>
2024-03-07 09:37:13 +0000

    Add 'split.go'

---
+++ split.go
@@ -0,0 +1,17 @@
+package piscine
+
+func Split(s, sep string) (tab []string) {
+	tmp_str := ""
+	for i := 0; i <= len(s)-len(sep); i++ {
+		if s[i:i+len(sep)] == sep {
+			tab = append(tab, tmp_str)
+			tmp_str = ""
+			i += len(sep) - 1
+		} else {
+			tmp_str += string(s[i])
+		}
+	}
+	tmp_str += string(s[len(s)-1])
+	tab = append(tab, tmp_str)
+	return tab
+}
