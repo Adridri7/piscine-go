8c6f4df3c7bd714c4649796c02acd18988910405
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-07 09:07:27 +0100

    Add

---
+++ appendrange.go
@@ -0,0 +1,8 @@
+package piscine
+
+func AppendRange(min, max int) (tab []int) {
+	for i := min; i <= max; i++ {
+		tab = append(tab, i)
+	}
+	return tab
+}
---
+++ flags/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	str_info :=
+		`--insert
+  -i
+         This flag inserts the string into the string passed as argument.
+--order
+  -o
+         This flag will behave like a boolean, if it is called it will order the argument.`
+
+	if len(args) == 0 || args[0] == "-h" || args[0] == "--help" {
+		fmt.Println(str_info)
+	} else if len(args) == 1 && args[0] != "--insert" && args[0] != "-i" && args[0] != "--order" && args[0] != "-o" {
+		fmt.Println(args[0])
+	}
+}
