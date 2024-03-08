d4c039374b7b8bbd4d36bae48dab6421865e0d88
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-08 10:27:16 +0100

    Update

--- split.go
+++ split.go
@@ -1,17 +1,11 @@
 package piscine
 
-func fonction(s string, sep string) []string {
-	//création d'un tableau
+func Split(s string, sep string) []string {
 	tab := []string{}
 	startindex := 0
 	Lsep := len(sep)
-
-	//boucle jusqu'a len - len du sep pour eviter runtime
 	for i := 0; i < len(s)-Lsep+1; i++ {
-
-		//BANGER
 		if s[i:i+Lsep] == sep {
-			//CLASSICO
 			tab = append(tab, s[startindex:i])
 			startindex = i + Lsep
 			i = i + Lsep - 1
