63097686c0c9ad2e0cf69d1136fe9ce8d7331486
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-11 09:48:19 +0100

    Add displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Print("File name missing")
+		return
+	}
+
+	if len(args) == 1 {
+		file, err := ioutil.ReadFile(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(file))
+	} else if len(args) > 1 {
+		fmt.Println("Too many arguments")
+	}
+}
