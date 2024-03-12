85becd48b055e6aaa8d5d9d08cef51f8548423d1
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-12 09:27:00 +0100

    Add issoted.go

---
+++ issorted.go
@@ -0,0 +1,22 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i <= len(a)-1; i++ {
+		for j := 0; j <= len(a)-1; j++ {
+			if check(a[i], a[j]) == -1 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func check(a, b int) int {
+	if a > b {
+		return 1
+	} else if a == b {
+		return 0
+	} else {
+		return -1
+	}
+}
