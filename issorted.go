55f6ef52841d35f56ffc32c1efaefc430b92913f
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-12 09:30:48 +0100

    Update

--- issorted.go
+++ issorted.go
@@ -2,13 +2,13 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i <= len(a)-1; i++ {
-		for j := 0; j <= len(a)-1; j++ {
+		for j := i + 1; j <= len(a)-1; j++ {
 			if check(a[i], a[j]) == -1 {
-				return true
+				return false
 			}
 		}
 	}
-	return false
+	return true
 }
 
 func check(a, b int) int {
