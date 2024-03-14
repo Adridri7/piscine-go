d6f5114fc253266992b93d40be4b6af91ebb47e7
arocchet <adrieen.rocchetti@gmail.com>
2024-03-14 17:08:03 +0100

    Update

--- enigma.go
+++ enigma.go
@@ -1,5 +1,12 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	***a, *b, *******c, ***d = *******c, ***a, ****d, b
+	tempA := ***a
+	tempB := *b
+	tempC := ******c
+	tempD := ****d
+	***a = *tempC
+	*b = tempA
+	******c = &tempD
+	****d = tempB
 }
