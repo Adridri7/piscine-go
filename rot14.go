3a522fc7307a054b75fa5a5ca4fe22a328f8d7d6
root <dev@01-edu.org>
2024-03-14 08:26:49 +0000

    Add 'rot14.go'

---
+++ rot14.go
@@ -0,0 +1,26 @@
+package piscine 
+
+func Rot14(s string) (res string) {
+  for _, char := range s{
+    if char == ' ' || char == '!' || char == '?'{
+      res += string(char)
+    } else{
+      if char >= 'a' && char <= 'z'{
+        if char + 14 > 'z'{
+          diff :=  char - 26 + 14
+          res += string(diff)
+        } else{
+          res += string(char + 14)
+        }
+      } else if char >= 'A' && char <= 'Z' {
+        if char + 14 > 'Z'{
+          diff := char + 14 - 26
+          res += string(diff)
+        } else{
+          res += string(char + 14)
+        }
+      }
+    }
+  }
+  return res
+}
