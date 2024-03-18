cfba85da2edc89afc71b23ce2a84c4f847877635
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-18 18:22:20 +0100

    Update

--- listmerge.go
+++ listmerge.go
@@ -1,5 +1,19 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	l1.Tail.Next = l2.Head
+	if l1.Head == nil {
+		l1.Head = l2.Head
+		l1.Tail = l2.Tail
+		return
+	} else if l2.Head == nil {
+		return
+	}
+
+	a := l1.Head
+	for a.Next != nil {
+		a = a.Next
+	}
+	a.Next = l2.Head
+	l1.Tail = l2.Tail
+
 }
