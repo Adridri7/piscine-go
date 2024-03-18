45b820e02588fd3442c9ed3d7643811ce73322c8
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-18 18:13:05 +0100

    Update

--- listmerge.go
+++ listmerge.go
@@ -1,5 +1,5 @@
 package piscine
 
 func ListMerge(l1 *List, l2 *List) {
-	l1.Tail = l2.Head
+	l1.Tail.Next = l2.Head
 }
--- listremoveif.go
+++ listremoveif.go
@@ -1,4 +1,4 @@
-package piscine 
+package piscine
 
 type NodeL struct {
 	Data interface{}
@@ -11,23 +11,23 @@ type List struct {
 }
 
 func ListRemoveIf(l *List, data_ref interface{}) {
-    if l.Head == nil {
-        return
-    }
-    for l.Head != nil && l.Head.Data == data_ref {
-        l.Head = l.Head.Next
-    }
-    if l.Head == nil {
-        l.Tail = nil
-        return
-    }
-    current := l.Head
-    for current.Next != nil {
-        if current.Next.Data == data_ref {
-            current.Next = current.Next.Next
-        } else {
-            current = current.Next
-        }
-    }
-    l.Tail = current
+	if l.Head == nil {
+		return
+	}
+	for l.Head != nil && l.Head.Data == data_ref {
+		l.Head = l.Head.Next
+	}
+	if l.Head == nil {
+		l.Tail = nil
+		return
+	}
+	current := l.Head
+	for current.Next != nil {
+		if current.Next.Data == data_ref {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+	l.Tail = current
 }
