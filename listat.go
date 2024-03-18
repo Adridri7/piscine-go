9bcfd9027823fd7508b8be7b2341547661abba95
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-18 18:11:26 +0100

    Update

--- listat.go
+++ listat.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListAt(l *NodeL, pos int) *NodeL {
 	if l == nil || pos < 0 {
 		return nil
