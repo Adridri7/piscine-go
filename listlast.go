342926ab23b74dddaf971abe05ff6f177e84f0fd
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-18 13:10:55 +0100

    Update

--- listlast.go
+++ listlast.go
@@ -1,5 +1,15 @@
 package piscine
 
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
 func ListLast(l *List) interface{} {
 	if l == nil || l.Tail == nil {
 		return nil
--- listsize.go
+++ listsize.go
@@ -1,15 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListSize(l *List) int {
 	cpt := 0
 	n := l.Head
