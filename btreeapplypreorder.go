2397701746084c712fbb919d4a1a94a6c4b699e0
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-19 10:40:43 +0100

    Update

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -4,10 +4,10 @@ func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	BTreeApplyPostorder(root.Right, f)
-	BTreeApplyPostorder(root.Left, f)
 	_, err := f(root.Data)
 	if err != nil {
 		return
 	}
+	BTreeApplyPreorder(root.Left, f)
+	BTreeApplyPreorder(root.Right, f)
 }
