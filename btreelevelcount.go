968803359e6ad71e3c43005d2777ade9804bf2fb
Adridri7 <adrieen.rocchetti@gmail.com>
2024-03-19 11:59:45 +0100

    Update

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -1,11 +1,9 @@
 package piscine
 
 func BTreeLevelCount(root *TreeNode) int {
-
 	if root == nil {
 		return 0
 	}
-
 	left := BTreeLevelCount(root.Left)
 	right := BTreeLevelCount(root.Right)
 	if left > right {
