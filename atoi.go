e2055d270c2b222b0e0653f0c8572b3e4e4ab9c9
root <dev@01-edu.org>
2024-04-28 09:36:09 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -1,38 +1,24 @@
 package piscine
 
-func Atoi(s string) int {
-	runes := []rune(s)
-	LenRune := 0
-	result := 0
-	for i := range runes {
-		LenRune = i + 1
-	}
-	if LenRune == 0 {
-		return 0
-	}
-	tens := 1
-	for k := 0; k < LenRune-1; k++ {
-		if runes[k] == '+' || runes[k] == '-' {
-			continue
-		}
-		tens *= 10
-	}
-	for i := range runes {
-		if (runes[0] == '-' || runes[0] == '+') && i == 0 {
-			continue
-		}
-		if runes[i] < '0' || runes[i] > '9' {
+func Atoi(s string) (res int) {
+	res = 0
+	sign := 1
+
+	for idx, char := range s {
+		if char >= '0' && char <= '9' || char == '-' || char == '+' {
+			if idx == 0 && char == '-' {
+				sign = -1
+			} else if idx == 0 && char == '+' {
+				sign = 1
+			} else if char == '-' || char == '+' && idx > 0 {
+				return 0
+			} else {
+				digit := char - '0'
+				res = res*10 + int(digit)
+			}
+		} else {
 			return 0
 		}
-		numb := 0
-		for j := '0'; j < runes[i]; j++ {
-			numb++
-		}
-		result += numb * tens
-		tens /= 10
 	}
-	if runes[0] == '-' {
-		result *= -1
-	}
-	return result
+	return res * sign
 }
